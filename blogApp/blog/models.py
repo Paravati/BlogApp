25a297a86cd54aaf4a4c32fc060029e0671b20cb
Patrycja <pati.iwanek@gmail.com>
2021-01-25 13:21:42 +0100

    updated models with publish manager

--- blogApp/blog/models.py
+++ blogApp/blog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -8,11 +13,13 @@ class Post(models.Model):
         ('draft', 'Roboczy'),
         ('published', 'Opublikowany'),
     )
+    objects = models.Manager()  # manager domyslny
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='published')
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
     body = models.TextField()
-    published = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
+    published = PublishedManager()   # manager niestandardowy   -> to było wcześniej
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
@@ -25,3 +32,8 @@ class Post(models.Model):
 
     def __str__(self):  # domyslna czytelna dla czlowieka reprezentacja obiekt
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                        args=[self.publish.year, self.publish.strftime('%m'),
+                              self.publish.strftime('%d'), self.slug])
