64a5bb51928e6447f04e83cb1d62a502e200b4fb
Patrycja <pati.iwanek@gmail.com>
2021-01-28 12:41:09 +0100

    implementation view based on class

--- blogApp/blog/urls.py
+++ blogApp/blog/urls.py
@@ -7,11 +7,7 @@ app_name = 'blog'
 
 urlpatterns = [
     # Widoki posta.
-    url(r'^$', views.post_list, name='post_list'),
-    # url(r'^$', views.PostListView.as_view(), name='post_list'),
+    # url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
 ]
-# urlpatterns = [
-#         url(r'', views.post_list, name='post_list'),
-#         url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', views.post_detail, name='post_detail'),
-#     ]
--- blogApp/blog/views.py
+++ blogApp/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
 def post_list(request):
@@ -13,7 +21,7 @@ def post_list(request):
         posts.paginator.page(1)  # jezeli zmienna page nie jest l.calkowita to pobierana jest pierwsza strona wynikow
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)  # jezeli zmienna page ma wartosc wieksza niz nr ostatniej strony wynikow to pobierana jest ostatnia strona wynikow
-    # posts = Post.published.all()
+
     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
