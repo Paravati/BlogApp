9ea2919df72f1de7a41e03328379cf871b513808
Patrycja <pati.iwanek@gmail.com>
2021-05-14 22:14:52 +0200

    sorting post by tag - tag is a link

--- blogApp/blog/views.py
+++ blogApp/blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -12,8 +13,14 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # po 3 posty na każdej stronie
     page = request.GET.get('page', 1)
     try:
@@ -23,7 +30,7 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)  # jezeli zmienna page ma wartosc wieksza niz nr ostatniej strony wynikow to pobierana jest ostatnia strona wynikow
 
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request,year, month,day, post):
